aee2c65e54dc7df4f9ab6cc18c8429060e7fef13
Myron Weber <mweber@northwoodadvisors.com>
2014-05-25 13:37:11 -0700

    refactor emails.py

--- 1avro/emails.py
+++ 1avro/emails.py
@@ -1,95 +1,62 @@
+import os
 import email
 import email.utils
 from HTMLParser import HTMLParser
 import time
 import avro.schema, avro.datafile, avro.io
 import pprint
+import timeit
 
 INPUT_DIR = '../data/emails'
 SCHEMA = './email.avsc'
 OUTPUT = '../data/emails.avro'
 
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
-
-class AvroMail(object):
-    def __init__(self, schema_file, output_file):
-        self.output_file = output_file
-        schema = avro.schema.parse(open(schema_file).read())
-        io_writer = avro.io.DatumWriter(schema)
-        self.data_writer = avro.datafile.DataFileWriter(
-                open(output_file, 'wb'),
-                io_writer,
-                schema
-                )
-
-    def write_message(self, msg_hash):
-        try:
-            self.data_writer.append(msg_hash)
-            print 'SUCCESS'
-        except Exception as e:
-            print str(e)
-            raise
-
-    def read_back(self):
-        self.data_writer.close()
-        io_reader = avro.io.DatumReader()
-        data_reader = avro.datafile.DataFileReader(
-                open(self.output_file),
-                io_reader
-                )
-        pp = pprint.PrettyPrinter()
-        for record in data_reader:
-            pp.pprint(record)
-        data_reader.close()
-
 def main():
+    # initialize the serialization handler
     avro_mail_handler = AvroMail(SCHEMA, OUTPUT)
+    # serialize the emails
     process_mails(avro_mail_handler)
+    # read them back for validation
     avro_mail_handler.read_back()
+    print "Parsing/Hashing Time:       %g seconds" % HASH_TIME.read()
+    print "Serialization/Writing Time: %g seconds" % WRITE_TIME.read()
 
 def process_mails(avro_mail_handler):
-    import os
+    counter = 0
     path = INPUT_DIR
+    # loop through all the message files
     for dir_entry in os.listdir(path):
         dir_entry_path = os.path.join(path, dir_entry)
         if os.path.isfile(dir_entry_path):
             with open(dir_entry_path, 'r') as my_file:
-                #print dir_entry_path
-                msg_hash = handle_mail(my_file.read())
+                # convert the message to a hash
+                HASH_TIME.start()
+                msg_hash = hash_message(my_file.read(), counter)
+                HASH_TIME.stop()
+
+                # pass the hash to the serialization handler
+                WRITE_TIME.start()
                 avro_mail_handler.write_message(msg_hash)
+                WRITE_TIME.stop()
 
-def handle_mail(mail):
-    msg_hash = hash_message(mail)
-    return msg_hash
+                counter += 1
 
-def hash_message(mail):
+def hash_message(mail, message_id):
     msg_hash = {}
     msg = email.message_from_string(mail)
 
     msg_subject = msg['Subject']
-
     msg_from = clean_address(msg.get_all('from', []))
-
     msg_to = clean_addresses(msg.get_all('to', []))
-
     msg_cc = clean_addresses(msg.get_all('cc', []))
-
     msg_date = clean_date(msg['Date'])
-
     msg_received = msg['Received']
-
     msg_content_type = msg.get_content_type()
     msg_charset = msg.get_content_charset()
     msg_payload = clean_payload(msg.get_payload(), msg_content_type, msg_charset)
 
     msg_hash = dict()
+    msg_hash['message_id'] = message_id
     msg_hash['subject'] =  msg_subject
     msg_hash['date'] = msg_date
     msg_hash['tos'] = msg_to
@@ -98,6 +65,7 @@ def hash_message(mail):
     msg_hash['body'] = msg_payload
     return msg_hash
 
+# clean a list of zero-to-many addresses
 def clean_addresses(addresses):
     retval = []
     addr_list  =  email.utils.getaddresses(addresses)
@@ -105,6 +73,7 @@ def clean_addresses(addresses):
         retval.append(address_scrub(address))
     return retval
 
+# clean a single address
 def clean_address(address):
     retval = {}
     address  =  email.utils.getaddresses(address)
@@ -136,9 +105,73 @@ def clean_payload(payload, content_type, charset):
     return body
 
 def strip_tags(html):
-    s = MLStripper()
+    s = TagRemover()
     s.feed(html)
     return s.get_data()
+
+class AvroMail(object):
+    def __init__(self, schema_file, output_file):
+        self.output_file = output_file
+        schema = avro.schema.parse(open(schema_file).read())
+        io_writer = avro.io.DatumWriter(schema)
+        self.data_writer = avro.datafile.DataFileWriter(
+                open(output_file, 'wb'),
+                io_writer,
+                schema
+                )
+
+    def write_message(self, msg_hash):
+        try:
+            self.data_writer.append(msg_hash)
+            # print 'SUCCESS'
+        except Exception as e:
+            print str(e)
+            raise
+
+    def read_back(self):
+        self.data_writer.close()
+        io_reader = avro.io.DatumReader()
+        data_reader = avro.datafile.DataFileReader(
+                open(self.output_file),
+                io_reader
+                )
+        pp = pprint.PrettyPrinter()
+        for record in data_reader:
+            pp.pprint(record)
+        data_reader.close()
+
+class Stopwatch(object):
+    def __init__(self):
+        self.reset()
+
+    def start(self):
+        self.start_time = time.time()
+        self.running = True
+
+    def stop(self):
+        if self.running:
+            self.elapsed_time = self.elapsed_time + (time.time() - self.start_time)
+        self.running = False
+
+    def read(self):
+        return self.elapsed_time
+    
+    def reset(self):
+        self.running = False
+        self.elapsed_time = 0.0
+
+
+class TagRemover(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+HASH_TIME = Stopwatch()
+WRITE_TIME = Stopwatch()
 
 if __name__ == "__main__":
     main()
